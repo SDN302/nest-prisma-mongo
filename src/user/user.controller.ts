a588df025a70f96c52972c4fb8e046e0abcbe859
Đinh Quốc Chương <quocchuong2609@gmail.com>
2024-09-22 08:22:46 +0700

    add: base for api

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { User } from '@prisma/client';
+import { updateUserDto, UserDto } from './dtos/user.dto';
+import { UserService } from './user.service';
 
 @Controller('users')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  // Vuong
+  @Post()
+  create(@Body() body: UserDto): Promise<User> {}
+
+  // Linh
+  @Get()
+  getAll() {}
+
+  // Linh
+  @Get(':id')
+  getById(@Param('id') id: string): Promise<User> {}
+
+  // Vuong
+  @Put(':id')
+  update(@Param('id') id: string, @Body() body: updateUserDto): Promise<User> {}
+
+  // Linh
+  @Delete(':id')
+  delete(@Param('id') id: string): Promise<User> {}
+}
