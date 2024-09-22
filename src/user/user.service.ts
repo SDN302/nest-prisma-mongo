066ebdeb8b584ae6cadbbed07947ce5451236ba4
Đinh Quốc Chương <quocchuong2609@gmail.com>
2024-09-22 08:26:13 +0700

    add: user service base

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { updateUserDto, UserDto } from './dtos/user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    // Linh
+    async getAll() {}
+    
+    // Linh
+    async getById(id: string) {}
+    
+    // Vuong
+    async update(id: string, body: updateUserDto) {}
+    
+    // Linh
+    async delete(id: string) {}
+    
+    // Vuong
+    async create(body: UserDto) {}
+}
